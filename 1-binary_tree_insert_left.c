aa1c0f071ab60d268057f5c4b4f1b33701c3b125
Only1allan <kariukiallan850@gmail.com>
2023-08-29 15:29:32 +0300

    add task one

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	newnode = malloc(sizeof(binary_tree_t));
 
-    if (parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	newnode = binary_tree_node(parent, value);
