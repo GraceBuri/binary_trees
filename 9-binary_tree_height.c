e8881060f8fd5f3100d2f3597470b9e97203fd6a
GraceBuri <grcbuuri@gmail.com>
2023-08-31 13:10:12 -0700

    add tasks for binary trees, second part

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,13 +6,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t i, j;
+	size_t i = 0, j = 0;
 
 	if (tree)
 	{
-		i = 0;
-		j = 0;
-
 		i = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		j = tree->right ? 1 + binary_tree_height(tree->right) :
 0;
